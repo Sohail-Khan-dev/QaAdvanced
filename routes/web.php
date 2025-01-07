1bcfb02cfd408549789ae5d54b062fabb15bc1c5
sohail-khan <sohail8338@gmail.com>
2025-01-07 23:41:42 +0500

    change folder name

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('qa.index');
 });
+Route::get('/{routename}', function ($routename) {
+    return view('qa.' . $routename);
+});
+
+
