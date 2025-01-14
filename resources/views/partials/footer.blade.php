2e3a38420578910aa835c7a895c5a4a4e282692c
zubair538 <zubairkhanlkl8338@gmail.com>
2025-01-14 23:53:06 +0500

    changes header footer, index page and image on index page

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,8 +5,8 @@
             <div class="col-md-6 col-lg-6 col-xl-3">
                 <div class="footer-item d-flex flex-column">
                     <div class="footer-item">
-                        <h4 class="text-white mb-4">JustDanc</h4>
-                        <p class="text-white mb-3">Dolor amet sit justo amet elitr clita ipsum elitr est.Lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit.</p>
+                        <h4 class="text-white mb-4">QaAdvance</h4>
+                        <p class="text-white mb-3">Enhance your testing skills with QAadvance. Your trusted platform for ISTQB preparation, providing comprehensive resources and guidance to help you excel in software quality assurance.</p>
                         <div class="d-flex">
                             <a class="btn btn-lg-square btn-primary rounded-circle me-2" href=""><i class="fab fa-facebook-f"></i></a>
                             <a class="btn btn-lg-square btn-primary rounded-circle mx-2" href=""><i class="fab fa-twitter"></i></a>
@@ -22,21 +22,21 @@
                     <div class="d-flex align-items-center mb-3">
                         <a class="btn btn-lg-square btn-primary rounded-circle mx-2" href=""><i class="fas fa-map-marker-alt"></i></a>
                         <div class="text-white ms-2">
-                            <p class="mb-0">0123.. Street, New York, USA</p>
+                            <p class="mb-0">Islamabad Pakistan</p>
                         </div>
                     </div>
                     <div class="d-flex align-items-center mb-3">
                         <a class="btn btn-lg-square btn-primary rounded-circle mx-2" href=""><i class="fa fa-phone-alt"></i></a>
                         <div class="text-white ms-2">
-                            <p class="mb-0">+012 345 67890</p>
-                            <p class="mb-0">+012 345 67890</p>
+                            <p class="mb-0">+923075000238</p>
+                            <p class="mb-0">+923469127630</p>
                         </div>
                     </div>
                     <div class="d-flex align-items-center">
                         <a class="btn btn-lg-square btn-primary rounded-circle mx-2" href=""><i class="fas fa-envelope"></i></a>
                         <div class="text-white ms-2">
-                            <p class="mb-0">info@example.com</p>
-                            <p class="mb-0">info@example.com</p>
+                            <p class="mb-0">info@qaadvance.com</p>
+                            <p class="mb-0">info@qaadvance.com</p>
                         </div>
                     </div>
                 </div>
@@ -45,10 +45,10 @@
                 <div class="footer-item d-flex flex-column">
                     <h4 class="text-white mb-4">Quick Links</h4>
                     <a href="#" class="footer-link"> About Us</a>
-                    <a href="#" class="footer-link"> Classes</a>
+                    <!-- <a href="#" class="footer-link"> Classes</a> -->
                     <a href="#" class="footer-link"> Privacy Policy</a>
                     <a href="#" class="footer-link"> Terms & Conditions</a>
-                    <a href="#" class="footer-link"> Schedule</a>
+                    <!-- //<a href="#" class="footer-link"> Schedule</a> -->
                     <a href="#" class="footer-link"> FAQ</a>
                     <a href="#" class="footer-link"> Contact Us</a>
                 </div>
@@ -57,7 +57,7 @@
                 <div class="footer-item d-flex flex-column">
                     <div class="footer-item">
                         <h4 class="text-white mb-4">Newsletter</h4>
-                        <p class="text-white mb-3">Dolor amet sit justo amet elitr clita ipsum elitr est.Lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit.</p>
+                        <p class="text-white mb-3">Unlock your potential with QAadvance! Stay ahead in the world of software testing with the latest ISTQB preparation resources, tips, and updates. Your journey to mastering software quality assurance starts here—join our community today!</p>
                         <div class="position-relative mx-auto rounded-pill">
                             <input class="form-control rounded-pill w-100 py-3 ps-4 pe-5" type="text" placeholder="Enter your email">
                             <button type="button" class="btn btn-primary rounded-pill position-absolute top-0 end-0 py-2 mt-2 me-2">SignUp</button>
@@ -82,7 +82,7 @@
                 <!--/*** This template is free as long as you keep the below author’s credit link/attribution link/backlink. ***/-->
                 <!--/*** If you'd like to use the template without the below author’s credit link/attribution link/backlink, ***/-->
                 <!--/*** you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". ***/-->
-                Designed By <a class="border-bottom text-white" href="https://htmlcodex.com">HTML Codex</a>
+                Designed By <a class="border-bottom text-white" href="https://htmlcodex.com">Sohail</a>
             </div>
         </div>
     </div>
