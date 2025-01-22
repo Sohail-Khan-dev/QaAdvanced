7cd35727554f860b6e1c7307b588e465fb9cb260
sohail-khan <sohail8338@gmail.com>
2025-01-22 21:23:26 +0500

    counter facts new file

--- resources/views/qa/blog.blade.php
+++ resources/views/qa/blog.blade.php
@@ -104,56 +104,8 @@
 
 
     <!-- Counter Facts Start -->
-    <div class="container-fluid counter-facts py-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-sm-6 col-md-6 col-xl-3 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="counter">
-                        <div class="counter-icon w-100 d-flex align-items-center justify-content-center">
-                            <h3>Dance Classes</h3>
-                        </div>
-                        <div class="counter-content d-flex align-items-center justify-content-center mt-4">
-                            <span class="counter-value" data-toggle="counter-up">158</span>
-                            <h4 class="text-secondary mb-0" style="font-weight: 600; font-size: 25px;">+</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-sm-6 col-md-6 col-xl-3 wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="counter">
-                        <div class="counter-icon w-100 d-flex align-items-center justify-content-center">
-                            <h3>Best Instructor</h3>
-                        </div>
-                        <div class="counter-content d-flex align-items-center justify-content-center mt-4">
-                            <span class="counter-value" data-toggle="counter-up">49</span>
-                            <h4 class="text-secondary mb-0" style="font-weight: 600; font-size: 25px;">+</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-sm-6 col-md-6 col-xl-3 wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="counter">
-                        <div class="counter-icon w-100 d-flex align-items-center justify-content-center">
-                            <h3>Total Brunch</h3>
-                        </div>
-                        <div class="counter-content d-flex align-items-center justify-content-center mt-4">
-                            <span class="counter-value" data-toggle="counter-up">17</span>
-                            <h4 class="text-secondary mb-0" style="font-weight: 600; font-size: 25px;">+</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-sm-6 col-md-6 col-xl-3 wow fadeInUp" data-wow-delay="0.7s">
-                    <div class="counter">
-                        <div class="counter-icon w-100 d-flex align-items-center justify-content-center">
-                            <h3>Happy customer</h3>
-                        </div>
-                        <div class="counter-content d-flex align-items-center justify-content-center mt-4">
-                            <span class="counter-value" data-toggle="counter-up">567</span>
-                            <h4 class="text-secondary mb-0" style="font-weight: 600; font-size: 25px;">+</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('partials.counter-facts')
+
     <!-- Counter Facts End -->
 
 
