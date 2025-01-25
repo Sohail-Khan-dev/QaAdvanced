7ffd7ed0b445ffc94526621714127f2858dcc6b9
sohail-khan <sohail8338@gmail.com>
2025-01-26 00:29:58 +0500

    TEst github Actions

--- resources/views/qa/blog.blade.php
+++ resources/views/qa/blog.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="bg-gray-100 text-gray-900">
     <div class="container mx-auto px-4 py-8">
-        <h1 class="text-4xl font-bold mb-6 text-center">QAAdvance Blog</h1>
+        <h1 class="text-4xl font-bold mb-6 text-center">QAAdvance Blog Written in Medium</h1>
         <div id="blog-container" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             <!-- Blog posts will be loaded here -->
         </div>
