49203bb6a34bfd7b36138a7ce9ba860955b06309
zubair538 <zubairkhanlkl8338@gmail.com>
2025-01-25 22:16:32 +0500

    blog, term and conduction

--- resources/views/qa/blog.blade.php
+++ resources/views/qa/blog.blade.php
@@ -1,115 +1,49 @@
 @extends('layout')
 
-@section('title', 'Blogs')
+@section('title', 'Contact us')
 
 @section('content')
-    <!-- Header Start -->
-    <div class="container-fluid bg-breadcrumb">
-        <div class="container text-center py-5" style="max-width: 900px;">
-            <h3 class="text-primary display-3 mb-4 wow fadeInDown" data-wow-delay="0.1s">Our Blog & News</h1>
-                <ol class="breadcrumb justify-content-center text-white mb-0 wow fadeInDown" data-wow-delay="0.3s">
-                    <li class="breadcrumb-item"><a href="index" class="text-dark">Home</a></li>
-                    <li class="breadcrumb-item"><a href="#" class="text-dark">Pages</a></li>
-                    <li class="breadcrumb-item active text-primary">Blogs</li>
-                </ol>
+<div class="bg-gray-100 text-gray-900">
+    <div class="container mx-auto px-4 py-8">
+        <h1 class="text-4xl font-bold mb-6 text-center">QAAdvance Blog</h1>
+        <div id="blog-container" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <!-- Blog posts will be loaded here -->
         </div>
     </div>
-    <!-- Header End -->
 
-    <!-- Banner Start -->
-    @include('partials.banner')
+    <script>
+        async function fetchMediumBlogs() {
+            const response = await fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@zubairkhansh');
+            const data = await response.json();
 
-    <!-- Banner End -->
+            const blogContainer = document.getElementById('blog-container');
+            blogContainer.innerHTML = '';
+            
+            if (data && data.items) {
+            // console.log(date);
 
-    <!-- Blogs Start -->
-    <div class="container-fluid blog py-5">
-        <div class="container py-5">
-            <div class="pb-5">
-                <h4 class="text-secondary sub-title fw-bold wow fadeInUp" data-wow-delay="0.1s">Blog & News</h4>
-                <h1 class="display-2 mb-0 wow fadeInUp" data-wow-delay="0.3s">Our Latest News & Articles</h1>
-            </div>
-            <div class="blog-carousel owl-carousel pt-5 wow fadeInUp" data-wow-delay="0.1s">
-                <div class="blog-item bg-white rounded wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="blog-img rounded-top">
-                        <img src="img/class-1.jpg" class="img-fluid rounded-top w-100" alt="Image">
-                    </div>
-                    <div class="bg-light rounded-bottom p-4">
-                        <div class="d-flex justify-content-between mb-4">
-                            <a href="#" class="text-muted"><i class="fa fa-calendar-alt text-primary"></i> 28 August
-                                2021</a>
-                            <a href="#" class="text-muted"><span class="fa fa-comments text-primary"></span> 3
-                                Comments</a>
-                        </div>
-                        <a href="#" class="h4 mb-3 d-block">Classical Dance class</a>
-                        <p class="mb-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni vero excepturi
-                            blanditiis quidem</p>
-                        <a class="btn btn-primary rounded-pill text-white py-2 px-4" href="#">Read More</a>
-                    </div>
-                </div>
-                <div class="blog-item bg-white rounded wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="blog-img rounded-top">
-                        <img src="img/class-2.jpg" class="img-fluid rounded-top w-100" alt="Image">
-                    </div>
-                    <div class="bg-light rounded-bottom p-4">
-                        <div class="d-flex justify-content-between mb-4">
-                            <a href="#" class="text-muted"><i class="fa fa-calendar-alt text-primary"></i> 28 August
-                                2021</a>
-                            <a href="#" class="text-muted"><span class="fa fa-comments text-primary"></span> 3
-                                Comments</a>
-                        </div>
-                        <a href="#" class="h4 mb-3 d-block">Classical Dance class</a>
-                        <p class="mb-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni vero excepturi
-                            blanditiis quidem</p>
-                        <a class="btn btn-primary rounded-pill text-white py-2 px-4" href="#">Read More</a>
-                    </div>
-                </div>
-                <div class="blog-item bg-white rounded wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="blog-img rounded-top">
-                        <img src="img/class-3.jpg" class="img-fluid rounded-top w-100" alt="Image">
-                    </div>
-                    <div class="bg-light rounded-bottom p-4">
-                        <div class="d-flex justify-content-between mb-4">
-                            <a href="#" class="text-muted"><i class="fa fa-calendar-alt text-primary"></i> 28 August
-                                2021</a>
-                            <a href="#" class="text-muted"><span class="fa fa-comments text-primary"></span> 3
-                                Comments</a>
-                        </div>
-                        <a href="#" class="h4 mb-3 d-block">Classical Dance class</a>
-                        <p class="mb-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni vero excepturi
-                            blanditiis quidem</p>
-                        <a class="btn btn-primary rounded-pill text-white py-2 px-4" href="#">Read More</a>
-                    </div>
-                </div>
-                <div class="blog-item bg-white rounded wow fadeInUp" data-wow-delay="0.7s">
-                    <div class="blog-img rounded-top">
-                        <img src="img/class-1.jpg" class="img-fluid rounded-top w-100" alt="Image">
-                    </div>
-                    <div class="bg-light rounded-bottom p-4">
-                        <div class="d-flex justify-content-between mb-4">
-                            <a href="#" class="text-muted"><i class="fa fa-calendar-alt text-primary"></i> 28 August
-                                2021</a>
-                            <a href="#" class="text-muted"><span class="fa fa-comments text-primary"></span> 3
-                                Comments</a>
-                        </div>
-                        <a href="#" class="h4 mb-3 d-block">Classical Dance class</a>
-                        <p class="mb-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni vero excepturi
-                            blanditiis quidem</p>
-                        <a class="btn btn-primary rounded-pill text-white py-2 px-4" href="#">Read More</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Blogs End -->
+                data.items.forEach(post => {
+                    console.log(post);
+                    
+                    const blogCard = document.createElement('div');
+                    blogCard.className = 'bg-white rounded-lg shadow-md p-4';
 
+                    blogCard.innerHTML = `
+                        <img src="${post.thumbnail}" alt="${post.title}" class="w-full h-48 object-cover rounded-t-lg">
+                        <h2 class="text-xl font-semibold mt-4">${post.title}</h2>
+                        <p class="text-gray-700 mt-2">${post.description.slice(0, 100)}...</p>
+                        <a href="${post.link}" target="_blank" class="text-blue-500 hover:underline mt-4 inline-block">Read More</a>
+                    `;
 
-    <!-- Counter Facts Start -->
-    @include('partials.counter-facts')
+                    blogContainer.appendChild(blogCard);
+                });
+            } else {
+                blogContainer.innerHTML = '<p class="text-center text-gray-500">No blogs found.</p>';
+            }
+        }
 
-    <!-- Counter Facts End -->
-
-
-    <!-- Training Start -->
-    @include('partials.training')
-    <!-- Training End -->
+        fetchMediumBlogs();
+    </script>
+</div>
 @endsection
+
---
+++ resources/views/qa/privacy-term.blade.php
@@ -0,0 +1,31 @@
+@extends('layout')
+
+@section('title', 'Terms & Conditions')
+<style>
+    
+        .terms-container {
+            max-width: 800px;
+            margin: auto;
+            margin-top: 7rem;
+        }
+    </style>
+@section('content')
+    <div class="terms-container">
+        <h1>Terms & Conditions</h1>
+        <p>Welcome to our website. If you continue to browse and use this website, you are agreeing to comply with and be bound by the following terms and conditions of use, which together with our privacy policy govern [Your Company Name]'s relationship with you in relation to this website. If you disagree with any part of these terms and conditions, please do not use our website.</p>
+        
+        <h2>Use of the Website</h2>
+        <p>The content of the pages of this website is for your general information and use only. It is subject to change without notice.</p>
+        
+        <h2>Privacy</h2>
+        <p>Your use of this website is also subject to our Privacy Policy. Please review our Privacy Policy, which also governs the website and informs users of our data collection practices.</p>
+        
+        <h2>Governing Law</h2>
+        <p>These terms and conditions are governed by and construed in accordance with the laws of QA Advance and you irrevocably submit to the exclusive jurisdiction of the courts in that State or location.</p>
+        
+        <h2>Changes to Terms</h2>
+        <p>We may update our Terms & Conditions from time to time. We will notify you of any changes by posting the new Terms & Conditions on this page.</p>
+        
+        <p>These terms and conditions were last updated on October 1, 2023.</p>
+    </div>
+@endsection
