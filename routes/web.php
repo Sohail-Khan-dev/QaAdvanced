5ac340f3fccc5ac5ebc96bdf276db6d4eabc2674
Sohail Khan <sohail8338@gmail.com>
2025-02-06 08:38:30 +0500

    Update web.php file for app-ads file

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('qa.index');
 });
-Route::get('/{routename}', function ($routename) {
-    return view('qa.' . $routename);
+Route::get('/app-ads.txt', function () {
+    return view('qa.index');
 });
 
+Route::get('/{routename}', function ($routename) {
+    $filePath = base_path($routename . '.txt'); // Look for the .txt file in the root folder
 
+    if (File::exists($filePath)) {
+        return response()->file($filePath, [
+            'Content-Type' => 'text/plain'
+        ]);
+    }
+     $viewPath = 'qa.' . $routename;
 
+    if (View::exists($viewPath)) {
+        return view($viewPath);
+    }
+
+    abort(404, 'View not found');
+    return view('qa.' . $routename);
+});
